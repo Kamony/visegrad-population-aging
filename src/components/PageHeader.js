ee655d426e931b8ad9c544af6efaac0b45fb85ef
Jirka H <jirihauser94@gmail.com>
2020-05-06 19:17:31 +0200

    centered page title

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const PageHeader = ({ title }) => (
-  <h2 className="has-text-weight-bold is-size-1 is-size-2-mobile is-medium is-primary page-header-tile">
+  <h2 className="has-text-weight-bold is-size-1 is-size-2-mobile is-medium is-primary page-header-tile has-text-centered">
     {title}
   </h2>
 );
