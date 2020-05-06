6c11ca8589657a498cbc267462f53836fd54255e
Jirka H <jirihauser94@gmail.com>
2020-05-06 19:12:57 +0200

    favicon + meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Visegrad 2020',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Visegrad project website 2020 - 2021 about population aging',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
